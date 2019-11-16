366350143e95bae2002f98be613a6f26718c87af
matus <matusino.sykora@gmail.com>
2019-11-16 13:55:27 +0100

    Initial Commit

---
+++ src/test/java/sk/Matus/Guru/Thymeleaf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.Matus.Guru.Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
